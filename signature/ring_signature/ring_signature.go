11241f9507cee066035dd6ab016a49b95f4e7338
CaptainShepard01 <49350081+CaptainShepard01@users.noreply.github.com>
2023-01-09 20:01:17 +0200

    Added documentation for RingSignature algorithm

--- signature/ring_signature/ring_signature.go
+++ signature/ring_signature/ring_signature.go
@@ -13,6 +13,8 @@ var (
 	curve = signature.NewCurve25519()
 )
 
+// RingSignature
+// https://bytecoin.org/old/whitepaper.pdf
 type RingSignature struct {
 	KeyImage *signature.Point
 	CList    []*big.Int
@@ -20,12 +22,15 @@ type RingSignature struct {
 }
 
 func SignMessage(message string, privateKey *big.Int, publicKey *signature.Point, publicKeys []*signature.Point, s int) (*RingSignature, error) {
+	// Define the size of the ring of public keys that will be used in signing
 	numberOfPKeys := len(publicKeys)
 
 	if s < 0 || s >= numberOfPKeys {
 		return nil, fmt.Errorf("wrong index of personal key")
 	}
 
+	// Pick random c[i] and r[i] from interval [1...N), i from 0 to n-1 inclusive, where n is size of the ring
+	// N is the prime order of the base point of the curve
 	cList := make([]*big.Int, numberOfPKeys)
 	rList := make([]*big.Int, numberOfPKeys)
 
@@ -40,68 +45,94 @@ func SignMessage(message string, privateKey *big.Int, publicKey *signature.Point
 			log.Panicln(err)
 		}
 	}
+
+	// This 2 arrays of elliptic curve points will be calculated in cycle and used in hash calculation
 	var lArray []*signature.Point
 	var rArray []*signature.Point
 
+	// Calculating so-called key image which identifies the key pair of signer
 	keyImage := signature.GetKeyImage(curve, publicKey, privateKey)
 
+	// lArray[i] = |r[i]*G, if i==s
+	//			   |r[i]*G + c[i]*P[i], else
+	// G is base point of the curve,
+	// s is signer's public key's ordinal number in the ring,
+	// P[i] is i-th public key in the ring.
+	//
+	// rArray[i] = |r[i]*H_p(P[i]), if i==s
+	//			   |r[i]*H_p(P[i]) + c[i]*I, else
+	// H_p(P[i]) is deterministic hash-function (Point on curve -> Point on curve) of i-th public key in the ring,
+	// I is previously calculated key image.
 	for i := 0; i < numberOfPKeys; i++ {
-		qI := new(big.Int).Set(rList[i])
+		rI := new(big.Int).Set(rList[i])
 
-		qG, err := curve.MulPoint(qI, curve.G())
+		rG, err := curve.MulPoint(rI, curve.G())
 		if err != nil {
 			log.Panicln(err)
 		}
 
-		qH, err := curve.MulPoint(qI, curve.ComputeDeterministicHash(publicKeys[i].Copy()))
+		rH, err := curve.MulPoint(rI, curve.ComputeDeterministicHash(publicKeys[i].Copy()))
 		if err != nil {
 			log.Panicln(err)
 		}
 
+		// lArray[i] = r[i]*G, if i==s
+		// rArray[i] = r[i]*H_p(P[i]), if i==s
 		if i == s {
-			lArray = append(lArray, qG)
-			rArray = append(rArray, qH)
+			lArray = append(lArray, rG)
+			rArray = append(rArray, rH)
 			continue
 		}
 
-		wI := new(big.Int).Set(cList[i])
+		cI := new(big.Int).Set(cList[i])
 
-		wP, err := curve.MulPoint(wI, publicKeys[i].Copy())
+		cP, err := curve.MulPoint(cI, publicKeys[i].Copy())
 		if err != nil {
 			log.Panicln(err)
 		}
 
-		qGwP, err := curve.AddPoint(qG, wP)
+		rGcP, err := curve.AddPoint(rG, cP)
 		if err != nil {
 			log.Panicln(err)
 		}
 
-		wIPoint, err := curve.MulPoint(wI, keyImage)
+		cIPoint, err := curve.MulPoint(cI, keyImage)
 		if err != nil {
 			log.Panicln(err)
 		}
 
-		qHwI, err := curve.AddPoint(qH, wIPoint)
+		rHcI, err := curve.AddPoint(rH, cIPoint)
 		if err != nil {
 			log.Panicln(err)
 		}
 
-		lArray = append(lArray, qGwP)
-		rArray = append(rArray, qHwI)
+		// lArray[i] = r[i]*G + c[i]*P[i], else
+		// rArray[i] = r[i]*H_p(P[i]) + c[i]*I, else
+		lArray = append(lArray, rGcP)
+		rArray = append(rArray, rHcI)
 	}
 
+	// Calculate non-interactive challenge with use of message and 2 previously calculated arrays
 	hash := getHash(message, lArray, rArray)
+	// Get *Int from this challenge
 	c := new(big.Int).SetBytes(hash[:])
 
+	// Calculate sum from 0 to ring size exclusive of c[i]
 	sum := new(big.Int)
 	for i := 0; i < numberOfPKeys; i++ {
 		if i != s {
 			sum = new(big.Int).Add(sum, cList[i])
 		}
 	}
-
+	// Get private key as copy in *Int data type
 	pKey := new(big.Int).Set(privateKey)
 
+	// c[s] = (c - sum) mod N
+	// c is previously calculated non-interactive challenge,
+	// N is the prime order of the base point of the curve.
+	//
+	// r[s] = r[s] - c[s]*pKey
+	// pKey is the signer's private key
 	cList[s] = new(big.Int).Mod(new(big.Int).Sub(c, sum), curve.N)
 	rList[s] = new(big.Int).Mod(new(big.Int).Sub(rList[s], new(big.Int).Mul(cList[s], pKey)), curve.N)
 
@@ -113,12 +144,23 @@ func SignMessage(message string, privateKey *big.Int, publicKey *signature.Point
 }
 
 func (sig *RingSignature) VerifySignature(message string, publicKeys []*signature.Point) bool {
+	// Define the size of the ring of public keys that will be used in signature
 	numberOfPKeys := len(publicKeys)
+
+	// This 2 arrays of elliptic curve points will be calculated in cycle and used in hash calculation
 	var newLArray []*signature.Point
 	var newRArray []*signature.Point
 
+	// A sum of c[i] in cList store in signature data structure (will be calculated)
 	cExpected := new(big.Int)
 
+	// newLArray[i] = r[i]*G + c[i]*P[i]
+	// G is base point of the curve,
+	// P[i] is i-th public key in the ring.
+	//
+	// newRArray[i] = r[i]*H_p(P[i]) + c[i]*I
+	// H_p(P[i]) is deterministic hash-function (Point on curve -> Point on curve) of i-th public key in the ring,
+	// I is previously calculated key image.
 	for i := 0; i < numberOfPKeys; i++ {
 		rI := new(big.Int).Set(sig.RList[i])
 
@@ -160,9 +202,15 @@ func (sig *RingSignature) VerifySignature(message string, publicKeys []*signatur
 		cExpected = new(big.Int).Mod(new(big.Int).Add(cExpected, sig.CList[i]), curve.N)
 	}
 
+	// Calculate non-interactive challenge with use of message and 2 previously calculated arrays
 	hash := getHash(message, newLArray, newRArray)
+	// Get *Int from this challenge, mod N
+	// N is the prime order of the base point of the curve.
 	cReal := new(big.Int).Mod(new(big.Int).SetBytes(hash[:]), curve.N)
 
+	// Compare the value got as sum of c[i] from signature data structure
+	// and the value calculated as non-interactive challenge.
+	// Verification must not pass if values are different.
 	return cReal.Cmp(cExpected) == 0
 }
 
