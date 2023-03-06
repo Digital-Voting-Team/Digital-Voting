468407b009f752f2e791e49fde4878ebb127c5d9
CaptainShepard01 <49350081+CaptainShepard01@users.noreply.github.com>
2023-03-06 19:45:32 +0200

    Adding transaction to block body function

--- block/body.go
+++ block/body.go
@@ -1,7 +1,12 @@
 package block
 
-type transaction any
+type transaction interface {
+}
 
 type Body struct {
 	Transactions []transaction `json:"transactions"`
+}
+
+func (b *Body) AddTransaction(myTransaction transaction) {
+	b.Transactions = append(b.Transactions, myTransaction)
 }
