8f18e42d6beaea3fb5af7812d33d11ae09da05b0
CaptainShepard01 <49350081+CaptainShepard01@users.noreply.github.com>
2023-03-10 19:35:35 +0200

    Added comment for possible future changes

--- back_end/transaction/transaction_interface.go
+++ back_end/transaction/transaction_interface.go
@@ -1,5 +1,6 @@
 package transaction
 
+// ITransaction TODO: think of moving interface into packages for their specific purposes (merkle_tree, block etc.)
 type ITransaction interface {
 	HashString() string
 	Print()
