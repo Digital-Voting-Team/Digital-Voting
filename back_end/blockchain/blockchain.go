ac024bd29600b8ef2a7b8b0f11657dc8a6e22a5c
Nazarii Savorona <nazar.savorona@gmail.com>
2023-03-22 20:04:13 +0200

    Add blockchain struct

--- back_end/blockchain/blockchain.go
+++ back_end/blockchain/blockchain.go
@@ -1,25 +1,7 @@
 package blockchain
 
-import (
-	"digital-voting/block"
-	"fmt"
-)
+import "digital-voting/block"
 
-// Blockchain TODO: create class Node with Blockchain and indexed databases
 type Blockchain struct {
 	Blocks []*block.Block
-}
-
-func (b *Blockchain) AddBlock(block *block.Block) {
-	b.Blocks = append(b.Blocks, block)
-}
-
-func (b *Blockchain) GetBlock(hash [32]byte) (*block.Block, error) {
-	for _, current := range b.Blocks {
-		if current.GetHash() == hash {
-			return current, nil
-		}
-	}
-
-	return nil, fmt.Errorf("block with given hash was not found")
 }
