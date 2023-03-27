a91023bf7099e265b541e2283590b5e4064b880e
CaptainShepard01 <49350081+CaptainShepard01@users.noreply.github.com>
2023-03-28 00:15:49 +0300

    Fixed data types names in block witness

--- back_end/block/witness.go
+++ back_end/block/witness.go
@@ -1,11 +1,16 @@
 package block
 
+import (
+	"digital-voting/signature/keys"
+	signature "digital-voting/signature/signatures/single_signature"
+)
+
 type Witness struct {
-	ValidatorsPublicKeys [][33]byte `json:"public_keys"`
-	ValidatorsSignatures [][65]byte `json:"signatures"`
+	ValidatorsPublicKeys []keys.PublicKeyBytes            `json:"public_keys"`
+	ValidatorsSignatures []signature.SingleSignatureBytes `json:"signatures"`
 }
 
-func (w *Witness) addSignature(publicKey [33]byte, signature [65]byte) {
+func (w *Witness) addSignature(publicKey keys.PublicKeyBytes, signature signature.SingleSignatureBytes) {
 	w.ValidatorsPublicKeys = append(w.ValidatorsPublicKeys, publicKey)
 	w.ValidatorsSignatures = append(w.ValidatorsSignatures, signature)
 }
