3d9c7291c768292efb6643e6c663c2b6e5232e95
RusikVolc <ruclanvolchetskyy1990@gmail.com>
2023-03-31 00:25:58 +0300

    Added block with voting and group creation and block with 2 votes and 1 anonymous vote to blockchain

--- back_end/cmd/main/main.go
+++ back_end/cmd/main/main.go
@@ -5,6 +5,7 @@ import (
 	"digital-voting/block"
 	"digital-voting/blockchain"
 	ip "digital-voting/identity_provider"
+	"digital-voting/signature/curve"
 	"digital-voting/signature/keys"
 	singleSignature "digital-voting/signature/signatures/single_signature"
 	"digital-voting/signer"
@@ -12,6 +13,7 @@ import (
 	stx "digital-voting/transaction/transaction_specific"
 	"digital-voting/validation"
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -66,9 +68,51 @@ func main() {
 	validator.AddBlockToChain(currentBlockchain, block1)
 	validator.ActualizeIdentityProvider(block1)
 
+	// Add second block with voting and group
+	whitelist := make([][33]byte, 0, len(identityProvider.UserPubKeys))
+	members := make([]keys.PublicKeyBytes, 0, len(identityProvider.UserPubKeys))
+	for k := range identityProvider.UserPubKeys {
+		whitelist = append(whitelist, k)
+		members = append(members, k)
+	}
+
+	txVoting := tx.NewTransaction(tx.VotingCreation, stx.NewTxVotingCreation(time.Now(),
+		"Black or White?",
+		[]string{"Black", "White", "Both"},
+		whitelist))
+	txGroup := tx.NewTransaction(tx.GroupCreation, stx.NewTxGroupCreation("IPS-41", members...))
+
+	txSigner.SignTransaction(adminKeyPair, txVoting)
+	txSigner.SignTransaction(adminKeyPair, txGroup)
+
+	validator.AddToMemPool(txVoting)
+	validator.AddToMemPool(txGroup)
+
+	block2 := validator.CreateBlock(block1.GetHash())
+	validator.AddBlockToChain(currentBlockchain, block2)
+	validator.ActualizeIdentityProvider(block2)
+
+	// Add third block with votings
+	txVote1 := tx.NewTransaction(tx.Vote, stx.NewTxVote(txVoting.GetHash(), 1))
+	txVote2 := tx.NewTransaction(tx.Vote, stx.NewTxVote(txVoting.GetHash(), 1))
+	txVote3 := stx.NewTxVoteAnonymous(txVoting.GetHash(), 2)
+
+	publicKeys := []*curve.Point{user1.GetPublicKey(), user2.GetPublicKey(), user3.GetPublicKey()}
+
+	txSigner.SignTransaction(user1, txVote1)
+	txSigner.SignTransaction(user2, txVote2)
+	txSigner.SignTransactionAnonymous(user3, publicKeys, 2, txVote3)
+
+	validator.AddToMemPool(txVote1)
+	validator.AddToMemPool(txVote2)
+	validator.AddToMemPool(txVote3)
+
+	block3 := validator.CreateBlock(block2.GetHash())
+	validator.AddBlockToChain(currentBlockchain, block3)
+	validator.ActualizeIdentityProvider(block3)
+
 	// Print blockchain
 	for _, b := range currentBlockchain.Blocks {
 		fmt.Printf("%v\n", b.Header)
 	}
-
 }
